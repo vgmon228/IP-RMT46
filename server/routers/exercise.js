b854acf6268dfa350a076ccd07f17a3450f204ae
vgmon228 <vgmon228@gmail.com>
2024-03-15 23:02:19 +0800

    feat:update exercise

--- server/routers/exercise.js
+++ server/routers/exercise.js
@@ -5,5 +5,6 @@ const ControllerExercise = require('../controllers/exercise')
 router.post('/exercise', ControllerExercise.createExercise)
 router.get('/exercise', ControllerExercise.showExercise)
 router.delete('/exercise/:id', ControllerExercise.deleteExercise)
+router.put('/exercise/:id', ControllerExercise.updateExercise)
 
 module.exports = router
